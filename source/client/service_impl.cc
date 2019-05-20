f2b16ca1873c4a0733ce57126386e80f07c430ed
Otto van der Schaaf <oschaaf@we-amp.com>
2019-05-20 16:59:37 +0200

    save
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- source/client/service_impl.cc
+++ source/client/service_impl.cc
@@ -2,12 +2,33 @@
 
 #include <grpc++/grpc++.h>
 
+#include "client/client.h"
+
 namespace Nighthawk {
 namespace Client {
 
 ::grpc::Status ServiceImpl::QueueSession(::grpc::ServerContext* context,
                                          const ::nighthawk::client::QueueSessionRequest* request,
                                          ::nighthawk::client::QueueSessionResponse* response) {
+  // nighthawk::client::CommandLineOptions options = request->options();
+  // TODO(oschaaf): translate CommandLineOptions to OptionsImpl.
+  // Main main(options);
+
+  std::vector<const char*> argv;
+  argv.push_back("foo");
+  argv.push_back("--concurrency");
+  argv.push_back("1");
+  argv.push_back("--duration");
+  argv.push_back("1");
+  argv.push_back("--rps");
+  argv.push_back("1");
+  argv.push_back("--verbosity");
+  argv.push_back("error");
+  std::string url = "http://127.0.0.1:10000/";
+  argv.push_back(url.c_str());
+
+  Main program(argv.size(), argv.data());
+  program.run();
   return grpc::Status::OK;
 }
 
@@ -22,6 +43,7 @@ ServiceImpl::ReconfigureSession(::grpc::ServerContext* context,
 ServiceImpl::IsSessionFinished(::grpc::ServerContext* context,
                                const ::nighthawk::client::IsSessionFinishedRequest* request,
                                ::nighthawk::client::IsSessionFinishedResponse* response) {
+
   return grpc::Status::OK;
 }
 
--- source/client/service_impl.h
+++ source/client/service_impl.h
@@ -4,6 +4,7 @@ namespace Nighthawk {
 namespace Client {
 
 class ServiceImpl final : public nighthawk::client::NighthawkService::Service {
+public:
   ::grpc::Status QueueSession(::grpc::ServerContext* context,
                               const ::nighthawk::client::QueueSessionRequest* request,
                               ::nighthawk::client::QueueSessionResponse* response) override;
