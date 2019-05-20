157e37afc7b014b7cb802d8a6bddb0a59caf8de0
Otto van der Schaaf <oschaaf@we-amp.com>
2019-05-20 23:00:10 +0200

    Save state
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- source/client/service_impl.cc
+++ source/client/service_impl.cc
@@ -10,25 +10,28 @@ namespace Client {
 ::grpc::Status ServiceImpl::QueueSession(::grpc::ServerContext* context,
                                          const ::nighthawk::client::QueueSessionRequest* request,
                                          ::nighthawk::client::QueueSessionResponse* response) {
+  response->mutable_session()->set_id("AAAA");
+
   // nighthawk::client::CommandLineOptions options = request->options();
   // TODO(oschaaf): translate CommandLineOptions to OptionsImpl.
   // Main main(options);
+  /*
+    std::vector<const char*> argv;
+    argv.push_back("foo");
+    argv.push_back("--concurrency");
+    argv.push_back("1");
+    argv.push_back("--duration");
+    argv.push_back("1");
+    argv.push_back("--rps");
+    argv.push_back("1");
+    argv.push_back("--verbosity");
+    argv.push_back("error");
+    std::string url = "http://127.0.0.1:10000/";
+    argv.push_back(url.c_str());
 
-  std::vector<const char*> argv;
-  argv.push_back("foo");
-  argv.push_back("--concurrency");
-  argv.push_back("1");
-  argv.push_back("--duration");
-  argv.push_back("1");
-  argv.push_back("--rps");
-  argv.push_back("1");
-  argv.push_back("--verbosity");
-  argv.push_back("error");
-  std::string url = "http://127.0.0.1:10000/";
-  argv.push_back(url.c_str());
-
-  Main program(argv.size(), argv.data());
-  program.run();
+    Main program(argv.size(), argv.data());
+    program.run();
+  */
   return grpc::Status::OK;
 }
 
