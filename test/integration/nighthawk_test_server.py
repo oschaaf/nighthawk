830ee98abb2e77dca660d643335c9db15ae14f7b
Otto van der Schaaf <oschaaf@we-amp.com>
2019-09-26 23:44:52 +0200

    save state
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- test/integration/nighthawk_test_server.py
+++ test/integration/nighthawk_test_server.py
@@ -1,9 +1,11 @@
+import http.client
 import json
 import logging
 import os
 import socket
 import subprocess
 import sys
+import random
 import tempfile
 import threading
 import time
@@ -17,7 +19,7 @@ class TestServerBase(object):
     Base class for running a server in a separate process.
     """
 
-  def __init__(self, server_binary_path, config_template_path, server_ip, server_port, ip_version,
+  def __init__(self, server_binary_path, config_template_path, server_ip, ip_version,
                server_binary_config_path_arg, parameters):
     assert ip_version != IpVersion.UNKNOWN
     self.ip_version = ip_version
@@ -27,13 +29,15 @@ class TestServerBase(object):
     self.server_process = None
     self.server_ip = server_ip
     self.socket_type = socket.AF_INET6 if ip_version == IpVersion.IPV6 else socket.AF_INET
-    self.server_port = server_port
+    self.server_port = -1
+    self.admin_port = -1
+    self.admin_address_path = ""
+    self.instance_id = str(random.randint(1,1024*1024*1024))
     self.parameters = parameters
     self.server_binary_config_path_arg = server_binary_config_path_arg
 
   def serverThreadRunner(self):
     self.parameters["server_ip"] = self.server_ip
-    self.parameters["server_port"] = self.server_port
 
     with open(self.config_template_path) as f:
       config = Template(f.read())
@@ -43,11 +47,12 @@ class TestServerBase(object):
     with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".yaml") as tmp:
       parameterized_config_path = tmp.name
       tmp.write(config)
+    with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".adminpath") as tmp:
+      self.admin_address_path = tmp.name
 
     args = [
         self.server_binary_path, self.server_binary_config_path_arg, parameterized_config_path,
-        "-l", "error", "--base-id",
-        str(self.server_port)
+        "-l", "error", "--base-id", self.instance_id, "--admin-address-path", self.admin_address_path
     ]
     logging.info("Test server popen() args: [%s]" % args)
     self.server_process = subprocess.Popen(args)
@@ -60,14 +65,21 @@ class TestServerBase(object):
       while tries > 0:
         time.sleep(0.5)
         tries -= 1
-        result = subprocess.run(
-            ["lsof", "-p",
-             str(self.server_process.pid), "-tni",
-             ":%s" % self.server_port],
-            stdout=subprocess.PIPE)
-        if result.stdout.decode('utf-8').strip() == str(self.server_process.pid):
-          if sock.connect_ex((self.server_ip, self.server_port)) == 0:
-            return True
+        with open(self.admin_address_path) as admin_address_file:
+          admin_address = admin_address_file.read()
+          self.admin_port = admin_address.split(":")[1]
+          admin_conn = http.client.HTTPConnection(admin_address)
+          admin_conn.request('GET', '/listeners?format=json')
+          admin_response = admin_conn.getresponse()
+          if not admin_response.status == 200:
+            return False
+          listeners = json.loads(admin_response.read().decode('utf-8'))
+          print (str(listeners))
+          self.server_port = listeners["listener_statuses"][0]["local_address"]["socket_address"]["port_value"]
+          print ("@@" + str(self.server_port))
+
+        if sock.connect_ex((self.server_ip, self.server_port)) == 0:
+          return True
       logging.error("Timeout while waiting for server listener at %s:%s to accept connections.",
                     self.server_ip, self.server_port)
       return False
@@ -93,8 +105,7 @@ class NighthawkTestServer(TestServerBase):
                server_binary_path,
                config_template_path,
                server_ip,
-               server_port,
                ip_version,
                parameters=dict()):
     super(NighthawkTestServer, self).__init__(server_binary_path, config_template_path, server_ip,
-                                              server_port, ip_version, "--config-path", parameters)
+                                              ip_version, "--config-path", parameters)
