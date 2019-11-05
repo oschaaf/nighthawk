fa19ac9394acaec3f26176b4c5874ee7c8f74f3e
Otto van der Schaaf <oschaaf@we-amp.com>
2019-11-05 14:37:59 +0100

    dump state
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- benchmarks/test_benchmark.py
+++ benchmarks/test_benchmark.py
@@ -11,20 +11,70 @@ from test.integration.integration_test_fixtures import (http_test_server_fixture
 from test.integration.utility import *
 
 
-def test_http_h1_maxrps_no_client_side_queueing(http_test_server_fixture):
+def test_http_h1_maxrps_1_connection(http_test_server_fixture):
   assert (http_test_server_fixture.test_server.enableCpuProfiler())
   MIN_EXPECTED_REQUESTS = 100
   parsed_json, _ = http_test_server_fixture.runNighthawkClient(
-      [http_test_server_fixture.getTestServerRootUri(), "--rps", "999999", "--duration", "30"])
+      [http_test_server_fixture.getTestServerRootUri(), "--rps", "25000", "--duration", "30", "--connections 1", "--concurrency 30"])
   counters = http_test_server_fixture.getNighthawkCounterMapFromJson(parsed_json)
   # We expect to have executed a certain amount of requests
   assertCounterGreater(counters, "benchmark.http_2xx", MIN_EXPECTED_REQUESTS)
   # We expect to have created only a single connection
-  assertCounterEqual(counters, "upstream_cx_http1_total", 2)
+  #assertCounterEqual(counters, "upstream_cx_http1_total", 2)
   global_histograms = http_test_server_fixture.getNighthawkGlobalHistogramsbyIdFromJson(parsed_json)
   assertGreater(int(global_histograms["sequencer.blocking"]["count"]), MIN_EXPECTED_REQUESTS)
   assertGreater(
       int(global_histograms["benchmark_http_client.request_to_response"]["count"]),
       MIN_EXPECTED_REQUESTS)
-  # dump output
-  logging.info(str(parsed_json))
+  logging.info("# Requests: %s" % str(counters["benchmark.http_2xx"]))
+
+def test_http_h2_maxrps_1_active_request(http_test_server_fixture):
+  assert (http_test_server_fixture.test_server.enableCpuProfiler())
+  MIN_EXPECTED_REQUESTS = 100
+  parsed_json, _ = http_test_server_fixture.runNighthawkClient(
+      [http_test_server_fixture.getTestServerRootUri(), "--rps", "25000", "--duration", "30", "--max-active-requests 1", "--h2", "--concurrency 30"])
+  counters = http_test_server_fixture.getNighthawkCounterMapFromJson(parsed_json)
+  # We expect to have executed a certain amount of requests
+  assertCounterGreater(counters, "benchmark.http_2xx", MIN_EXPECTED_REQUESTS)
+  # We expect to have created only a single connection
+  #assertCounterEqual(counters, "upstream_cx_http1_total", 2)
+  global_histograms = http_test_server_fixture.getNighthawkGlobalHistogramsbyIdFromJson(parsed_json)
+  assertGreater(int(global_histograms["sequencer.blocking"]["count"]), MIN_EXPECTED_REQUESTS)
+  assertGreater(
+      int(global_histograms["benchmark_http_client.request_to_response"]["count"]),
+      MIN_EXPECTED_REQUESTS)
+  logging.info("# Requests: %s" % str(counters["benchmark.http_2xx"]))
+
+def test_http_h1_maxrps_100_connection(http_test_server_fixture):
+  assert (http_test_server_fixture.test_server.enableCpuProfiler())
+  MIN_EXPECTED_REQUESTS = 100
+  parsed_json, _ = http_test_server_fixture.runNighthawkClient(
+      [http_test_server_fixture.getTestServerRootUri(), "--rps", "25000", "--duration", "30", "--connections 100", "--concurrency 30"])
+  counters = http_test_server_fixture.getNighthawkCounterMapFromJson(parsed_json)
+  # We expect to have executed a certain amount of requests
+  assertCounterGreater(counters, "benchmark.http_2xx", MIN_EXPECTED_REQUESTS)
+  # We expect to have created only a single connection
+  #assertCounterEqual(counters, "upstream_cx_http1_total", 2)
+  global_histograms = http_test_server_fixture.getNighthawkGlobalHistogramsbyIdFromJson(parsed_json)
+  assertGreater(int(global_histograms["sequencer.blocking"]["count"]), MIN_EXPECTED_REQUESTS)
+  assertGreater(
+      int(global_histograms["benchmark_http_client.request_to_response"]["count"]),
+      MIN_EXPECTED_REQUESTS)
+  logging.info("# Requests: %s" % str(counters["benchmark.http_2xx"]))
+
+def test_http_h2_maxrps_100_active_request(http_test_server_fixture):
+  assert (http_test_server_fixture.test_server.enableCpuProfiler())
+  MIN_EXPECTED_REQUESTS = 100
+  parsed_json, _ = http_test_server_fixture.runNighthawkClient(
+      [http_test_server_fixture.getTestServerRootUri(), "--rps", "25000", "--duration", "30", "--max-active-requests 100", "--h2", "--concurrency 30"])
+  counters = http_test_server_fixture.getNighthawkCounterMapFromJson(parsed_json)
+  # We expect to have executed a certain amount of requests
+  assertCounterGreater(counters, "benchmark.http_2xx", MIN_EXPECTED_REQUESTS)
+  # We expect to have created only a single connection
+  #assertCounterEqual(counters, "upstream_cx_http1_total", 2)
+  global_histograms = http_test_server_fixture.getNighthawkGlobalHistogramsbyIdFromJson(parsed_json)
+  assertGreater(int(global_histograms["sequencer.blocking"]["count"]), MIN_EXPECTED_REQUESTS)
+  assertGreater(
+      int(global_histograms["benchmark_http_client.request_to_response"]["count"]),
+      MIN_EXPECTED_REQUESTS)
+  logging.info("# Requests: %s" % str(counters["benchmark.http_2xx"]))
