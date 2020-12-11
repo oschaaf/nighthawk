2065453dbbbe2272a40c9e1f27447fa1c856be0d
Otto van der Schaaf <oschaaf@we-amp.com>
2020-12-11 15:01:27 +0100

    save state
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

---
+++ source/common/nighthawk_sink_client_impl.h
@@ -0,0 +1,25 @@
+#include "nighthawk/common/nighthawk_sink_client.h"
+
+#include "external/envoy/source/common/common/statusor.h"
+#include "external/envoy/source/common/protobuf/protobuf.h"
+
+#include "api/client/options.pb.h"
+#include "api/client/service.grpc.pb.h"
+
+namespace Nighthawk {
+
+/**
+ * Real implementation of a helper that opens a channel with the gRPC stub, sends the input, and
+ * translates the output or errors into a StatusOr.
+ *
+ * This class is stateless and may be called from multiple threads. Furthermore, the same gRPC stub
+ * is safe to use from multiple threads simultaneously.
+ */
+class NighthawkSinkClientImpl : public NighthawkSinkClient {
+public:
+  absl::StatusOr<nighthawk::client::StoreExecutionResponse> StoreExecutionResponseStream(
+      nighthawk::client::NighthawkSink::StubInterface* nighthawk_sink_stub,
+      const nighthawk::client::StoreExecutionRequest& store_execution_request) const override;
+};
+
+} // namespace Nighthawk
